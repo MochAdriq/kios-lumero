08a4f96870e070e68400641806cb17b4907a4c23
Moch Adriq <mochfadillah1208@gmail.com>
2026-07-18 14:20:47 +0700

    fix: add missing session_start in member portal files

--- member/index.php
+++ member/index.php
@@ -1,4 +1,5 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) session_start();
 require_once __DIR__ . '/../helpers/functions.php';
 require_once __DIR__ . '/../core/Database.php';
 $pdo = Database::connection();
