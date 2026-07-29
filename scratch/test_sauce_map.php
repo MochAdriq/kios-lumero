148496959887e2baace822a1aad9d70864abe525
Moch Adriq <mochfadillah1208@gmail.com>
2026-07-29 12:34:51 +0700

    fix(pos): map bbq spicy and italian barbeque correctly

---
+++ scratch/test_sauce_map.php
@@ -0,0 +1,47 @@
+<?php
+$rawVars = file_get_contents('scratch/query_ayam_vars.php');
+// The output of query_ayam_vars.php was saved, let's just use the known list:
+$vars = [
+'Dada Original Tanpa Nasi',
+'Dada BBQ Spicy Tanpa Nasi',
+'Dada Keju Tanpa Nasi',
+'Dada Lada Hitam Tanpa Nasi',
+'Dada Sadis Tanpa Nasi',
+'Dada Sambal Geprek Tanpa Nasi',
+'Dada Mentai Tanpa Nasi',
+'Dada Teriyaki Tanpa Nasi',
+'Dada Geprek Extra Mozzarella Tanpa Nasi',
+'Dada Geprek Extra Mentai Tanpa Nasi',
+'Dada Garlic Tanpa Nasi'
+];
+
+$sauceDefs = [
+    ['key'=>'keju', 'match'=>['keju', 'cheese']],
+    ['key'=>'lada_hitam', 'match'=>['lada hitam', 'black pepper', 'blackpepper']],
+    ['key'=>'garlic', 'match'=>['garlic', 'bawang', 'sicilian']],
+    ['key'=>'teriyaki', 'match'=>['teriyaki']],
+    ['key'=>'sadis_mozzarella', 'match'=>['smashed chili extra mozzarella', 'geprek extra mozzarella']],
+    ['key'=>'sadis_mentai', 'match'=>['smashed chili extra mentai', 'geprek extra mentai']],
+    ['key'=>'sadis', 'match'=>['sadis', 'geprek', 'pedas', 'spicy', 'smashed chili']],
+    ['key'=>'bbq', 'match'=>['bbq', 'barbeque']],
+    ['key'=>'mentai', 'match'=>['mentai', 'mayo', 'mayonnaise']],
+    ['key'=>'picante', 'match'=>['picante']],
+    ['key'=>'mozzarella', 'match'=>['mozzarella']],
+    ['key'=>'carbonara', 'match'=>['carbonara']]
+];
+
+$found = [];
+foreach($vars as $v) {
+    $v = strtolower($v);
+    $matched = false;
+    foreach($sauceDefs as $def) {
+        foreach($def['match'] as $m) {
+            if(strpos($v, $m) !== false) {
+                $found[$def['key']] = true;
+                $matched = true;
+                break 2;
+            }
+        }
+    }
+}
+print_r(array_keys($found));
