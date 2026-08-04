7244fa742d496acc79b470ad83d2e4e1976cbf92
Moch Adriq <mochfadillah1208@gmail.com>
2026-08-04 11:43:07 +0700

    fix(core): sinkronisasi timezone MySQL dengan aplikasi PHP

--- core/Database.php
+++ core/Database.php
@@ -7,10 +7,17 @@ class Database
         if (self::$pdo === null) {
             $config = require __DIR__ . '/../config/database.php';
             $dsn = sprintf('mysql:host=%s;port=%s;dbname=%s;charset=%s', $config['host'], $config['port'], $config['database'], $config['charset']);
+            // Mengambil timezone offset berdasarkan konfigurasi aplikasi (.env)
+            $tz = app_env('APP_TIMEZONE', 'Asia/Jakarta');
+            $dtz = new DateTimeZone($tz);
+            $dt = new DateTime("now", $dtz);
+            $offset = $dt->format("P"); // Output: '+07:00'
+
             self::$pdo = new PDO($dsn, $config['username'], $config['password'], [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone = '{$offset}'"
             ]);
             
             // Auto-install: Jika tabel users belum ada, import database/dcc_export.sql otomatis
